df14c502b335ccfc28e5d3d71d6bda39611aacc4
Mateus <mateus.oliveira@ccc.ufcg.edu.br>
2019-05-15 20:46:56 -0300

    Fazendo métodos provisorios em MainProject

--- Haskell/src/MainProject.hs
+++ Haskell/src/MainProject.hs
@@ -17,6 +17,37 @@ editToDo projectName = do
 filterTodo :: String -> IO()
 filterTodo projectName = mainFilter projectName
 
+createToDo :: String -> IO()
+createToDo projectName = do
+    putStrLn("Criar ToDo\n")
+    putStrLn("Digite o nome da ToDo: ")
+    name <- getLine
+
+    putStrLn("Descricao da ToDo: ")
+    description <- getLine
+
+    putStrLn("Digite o nome dos responsaveis: ")
+    responsible <- getLine
+
+    putStrLn("Digite a duração da ToDo (em dias): ")
+    duration <- getLine
+
+    concludeScreen("ToDo criado");
+
+editProjectName :: String -> IO()
+editProjectName projectName = do
+    putStrLn "Editar nome do Projeto"
+    putStrLn ("Nome atual: " ++ projectName)
+    putStrLn "Digite o novo nome: "
+    newName <- getLine
+    --pegar o nome de vdd
+    if(projectName == newName) then
+        putStrLn "Os nomes dos Projetos são iguais"
+    else
+        putStrLn "Ok"
+    -- setar nome do projeto
+    concludeScreen("Nome do ToDo atualizado")
+
 mainProject :: String -> IO()
 mainProject projectName = do 
     clear
@@ -32,11 +63,11 @@ mainProject projectName = do
     choice <- getLine
 
     case choice of
-               "1" -> putStrLn "ok" 
+               "1" -> createToDo projectName
                "2" -> editToDo projectName
                "3" -> putStrLn "ok"
                "4" -> filterTodo projectName
                "5" -> putStrLn "ok"
-               "6" -> putStrLn "ok"
+               "6" -> editProjectName projectName
                "0" -> returnScreen
                _   -> mainProject projectName
