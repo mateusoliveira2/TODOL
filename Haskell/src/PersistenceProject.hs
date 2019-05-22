6a5521804e2dc4a38d1313c8bc7cfce8f2d9512f
João Marcelo <marcelinhofernandes2013@gmail.com>
2019-05-21 21:01:41 -0300

    Finalizando metodos de persistenceController.

--- Haskell/src/PersistenceProject.hs
+++ Haskell/src/PersistenceProject.hs
@@ -16,9 +16,17 @@ criaDiretorio :: String -> IO()
 criaDiretorio nome = do
     success <- doesDirectoryExist nome
     
-    if success then return () 
+    if success 
+    then return () 
     else createDirectory nome
- 
+
+-- Recebe uma lista, a posicao, e um novo elemento para a posicao.
+alteraLista :: [t] -> Int -> t -> [t]
+alteraLista lista indice elemento = do
+    parte1 <- take (indice - 1)
+    parte2 <- drop indice
+    parte1 ++ elemento ++ parte2 
+
 persistirProjeto :: String -> String -> String -> String -> Int -> String -> IO()
 persistirProjeto nome descricao responsavel status previsao deta = do
     criaDiretorio "../Projects"
@@ -31,13 +39,33 @@ setNomeProjeto :: String -> String -> IO()
 setNomeProjeto nome novoNome = do
     let diretorioAntigo = "../Projects/" ++ nome ++ "/"
     let diretorioNovo = "../Projects/" ++ novoNome ++ "/"
+    projeto <- returnProjeto nome
+    lista <- alteraLista projeto 1 nome
     renameFile (diretorioAntigo ++ nome ++ ".txt") (diretorioAntigo ++ novoNome ++ ".txt")
     renameDirectory (diretorioAntigo) (diretorioNovo)
+
+setDescricaoProjeto :: String -> String -> [String]
+setDescricaoProjeto nome descricao = do
+    projeto <- returnProjeto nome
+    lista <- alteraLista projeto 2 descricao
     
--- setStatusProjeto :: String -> String -> ()
+setResponsavelProjeto :: String -> String -> [String]
+setResponsavelProjeto nome responsavel = do
+    projeto <- returnProjeto nome
+    lista <- alteraLista projeto 3 responsavel
 
--- setDescricaoProjeto :: String -> String -> ()
+setStatusProjeto :: String -> String -> [String]
+setStatusProjeto nome status = do
+    projeto <- returnProjeto nome
+    lista <- alteraLista projeto 4 status
 
--- setResponsavelProjeto :: String -> String -> ()
+setDataProjeto :: String -> String -> [String]
+setDataProjeto nome deta = do
+    projeto <- returnProjeto nome
+    lista <- alteraLista projeto 6 status
 
--- setDataProjeto :: String -> String -> ()
+returnProjeto :: String -> [String]
+returnProjeto nomeProjeto = do
+    let nome = "../Projects/" ++ nomeProjeto ++ "/" ++ nomeProjeto ++ ".txt"
+    contents <- readFile nome
+    lines contents
