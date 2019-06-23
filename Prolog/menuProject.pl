fc8a3563d0e7ad4b7f5f92d64b56765a4d3de092
Mateus Oliveira <33286442+mateusoliveira2@users.noreply.github.com>
2019-06-23 14:09:10 -0300

    Adicionando modulo ToDo

--- Prolog/menuProject.pl
+++ Prolog/menuProject.pl
@@ -1,4 +1,5 @@
 :- module(menuProject, [menuProject/1, receiverToDoData/1, showProject/0]).
+:- use_module(menuToDo).
 
 menuProject(ProjectName):- repeat,
         write("\n\n ------- "), write(ProjectName), write(" ------- \n\n"),
@@ -13,31 +14,40 @@ menuProject(ProjectName):- repeat,
         read(Choice),
         
         ( Choice = 0 -> !, fail ; true ),
-        (Choice = 1 -> receiverToDoData(ProjectName) ; true),
+        ( Choice = 1 -> receiverToDoData(ProjectName) ; true),
+        ( Choice = 2 -> editToDo(ProjectName); true),
+        ( Choice = 6 -> editProjectName(ProjectName); true),
         
         fail.
 
 receiverToDoData(ProjectName):-
         write("\n\n-------CRIAR TODO -------\n"),
         write("Digite o nome da ToDO: \n"),
-        read(TodoName),
+        read(ToDoName),
         write("Digite a descrição da ToDo: \n"),
         read(Description),
         write("Digite os responsaveis pela ToDo: \n"),
         read(Responsible),
         write("Digite a duração (em horas) da ToDo: \n"),
         read(Duration),
-        write(TodoName), write(" "), write(ProjectName),write(" ") , write(Description), write(" "),write(Responsible),
+        write(ToDoName), write(" "), write(ProjectName),write(" ") , write(Description), write(" "),write(Responsible),
         write(" "), write(Duration),
         
         menuProject(ProjectName).
 
-selectProject:-
-        write("\n\n-------SELECIONAR PROJETO-------\n"),
-        write("Digite o nome do projeto: \n"),
-        read(Name),
-        write(Name),write(' selecionado com sucesso!').
+editToDo(ProjectName):-
+        write("\n\n-------EDITAR TODO-------\n"),
+        write("Digite o nome da ToDo: \n"),
+        read(ToDoName),
+        write(ToDoName),write(' selecionado com sucesso!'),
+        menuToDo(ProjectName, ToDoName).
 
+editProjectName(ProjectName):-
+        write("\n\n-------EDITAR NOME DO PROJETO-------\n"),
+        write("Nome atual: "), write(ProjectName),
+        write("\n Digite o novo nome: "),
+        read(NewProjectName),
+        write("\nNome atualizado com sucesso. Novo nome: "), write(NewProjectName).
 
 showProject:-
 	write("\n\n┌────────────────────────────────────────────────\n"),
