a91077cf79e752f1fe95e129a5cd8beddde67144
Gabrielgtt <gabrielgtt@hotmail.com>
2019-06-27 18:22:20 -0300

    Refatorando e adicionando método de recuperação de Projeto na persistência do Projeto

--- Prolog/persistenceProject.pl
+++ Prolog/persistenceProject.pl
@@ -1,24 +1,41 @@
-:- module(persistenceProject, [persistirProjeto/5]).
+:- module(persistenceProject, [persistirProjeto/5, recuperaProjeto/5]).
 
 createDirectory(Directory):- exists_directory(Directory) -> true; make_directory(Directory).
 
+urlProject(Nome, Caminho) :-
+	string_concat("Projects/", Nome, Caminho).
+
+urlProjectFile(Nome, Caminho):-
+	urlProject(Nome, UrlProjeto),
+	string_concat(UrlProjeto, "/", Auxiliar),
+	string_concat(Auxiliar, Nome, Caminho).
+
 projectExists(Project) :- 
-	string_concat("Projects/", Project, Path),
+	urlProject(Project, Path),
 	exists_directory(Path).
 
 persistirProjeto(Nome, Descricao, Responsavel, Status, Previsao) :- 
     createDirectory("Projects"),
-    string_concat("Projects/", Nome, NomeDiretorio),
-    createDirectory(NomeDiretorio),
-    string_concat(NomeDiretorio, "/", Auxiliar),
-    string_concat(Auxiliar, Nome, Caminho),
+	urlProject(Nome, CaminhoDiretorio),
+    createDirectory(CaminhoDiretorio),
+	urlProjectFile(Nome, CaminhoProjeto),
 
-    open(Caminho, write, P),
+    open(CaminhoProjeto, write, P),
     
-    write(P, Nome), write(P, "\n"),
-    write(P, Descricao), write(P, "\n"),
-    write(P, Responsavel), write(P, "\n"),
-    write(P, Status), write(P, "\n"),
-    write(P, Previsao), write(P, "\n"),
+    write(P, Nome), write(P, ".\n"),
+    write(P, Descricao), write(P, ".\n"),
+    write(P, Responsavel), write(P, ".\n"),
+    write(P, Status), write(P, ".\n"),
+    write(P, Previsao), write(P, ".\n"),
 
     close(P).
+
+recuperaProjeto(NomeProjeto, Descricao, Responsavel, Status, Previsao) :-
+	urlProjectFile(NomeProjeto, Caminho),
+	open(Caminho, read, Str),
+	read(Str, _),
+	read(Str, Descricao),
+	read(Str, Responsavel),
+	read(Str, Status),
+	read(Str, Previsao),
+	close(Str).
